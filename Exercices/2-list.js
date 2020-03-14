53fd559017e50f62a952890f7acbb50f05b5f49e
jaime <jaime.molina@mattersupply.co>
2020-03-13 20:17:00 -0500

    2-list

--- Exercices/2-list.js
+++ Exercices/2-list.js
@@ -5,3 +5,55 @@
  * Crear un metodo que me haga la busqueda por ranking
  * "menor o igual" a 10
  */
+
+const Movies = require('./0-Db')
+
+const id = 'tt0095327'
+
+Movies.findOne({ id }, (err, movie) => {
+  if (err) {
+    console.error(err)
+  }
+
+  console.log('TCL: findOne => movie', movie)
+})
+
+const _id = '5e6ae0c561fc166a145815f6'
+Movies.findById(_id, (err, movie) => {
+  if (err) {
+    console.error(err)
+  }
+
+  console.log('TCL: findById => movie', movie)
+})
+
+// -----
+
+Movies.find({ rank: { $lte: 10 } }, (err, movies) => {
+  if (err) {
+    console.error(err)
+  }
+
+  const shortenMovies = movies.map(movie => ({
+    title: movie.title,
+    rank: movie.rank
+  }))
+  console.log('TCL: find => movies', shortenMovies)
+})
+
+// { rank: { $lte: 10, $mod: [2, 0] } },
+Movies.find(
+  { $and: [{ rank: { $lte: 10 } }, { rank: { $mod: [2, 0] } }] },
+
+  (err, movies) => {
+    if (err) {
+      console.error(err)
+    }
+
+    const shortenMovies = movies.map(movie => ({
+      title: movie.title,
+      rank: movie.rank
+    }))
+    console.log('TCL: find => movies', shortenMovies)
+  }
+)
